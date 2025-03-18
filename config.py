d1f0f6696713d5138f388f10a353b36825cf05a0
4-0-4 <146018190+nyeinkokoaung404@users.noreply.github.com>
2025-03-19 08:34:39 +0900

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python3
+# coding: utf-8
+###########################################
+# Copyright (C) t.me/nkka404
+# Channel: https://t.me/premium_channel_404
+###########################################
+# Pyrogram setup
+API_ID = "123456"  # Replace this API ID with your actual API ID
+API_HASH = "aeiou123"  # Replace this API HASH with your actual API HASH
+BOT_TOKEN = "7740747124:xxxxxxx"  # Replace this BOT_TOKEN
+# Admin IDs
+ADMIN_IDS = [12345, 789010]
